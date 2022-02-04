98227231bbf2ec752dcefaa0b6cf5cb82cc0571b
ivnuk <ihor.vnukov@ab-inbev.com>
2022-02-04 21:48:55 +0200

    Add lesson2.py

---
+++ lesson2_dz.py
@@ -0,0 +1,9 @@
+class Employee:
+    def work(self):
+        return 'I come to the office'
+
+class Programmer(Employee):
+    def work(self):
+        """
+        call super and add extra symbols
+        """
